1eaa59fd2a5bc37c790292e5bff155ecea3be4d2
breaun1 <breaun1@gmail.com>
2023-11-25 17:28:09 -0600

    identifying the information and gathering data about the plants

---
+++ script.js
@@ -0,0 +1,6 @@
+let radius =  3.14 * 5 * 5; //Total area of space
+let plantSpace = 0.8 *0.8; //Space per plant 
+let numOfPlants = 20; //Starting number of plants
+let weekOne = numOfPlants * 2;
+let weekTwo = weekOne * 2;
+let weekThree = weekTwo * 2;
